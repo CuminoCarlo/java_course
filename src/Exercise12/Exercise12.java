ccd336fd716539a473490b51ff162623e3213ed6
Carlo Cumino <ccumino89@gmail.com>
2024-10-05 18:11:14 +0200

    Add the exercise about execeptions

---
+++ src/Exercise12/Exercise12.java
@@ -0,0 +1,17 @@
+package Exercise12;
+
+public class Exercise12 {
+
+    public static void main(String[] args) {
+
+        try{
+            int c= 30/0;
+            System.out.println("The result is: "+c);
+        } catch (ArithmeticException e) {
+            System.out.println("Impossibile dividing for 0");
+            e.printStackTrace();
+        } finally {
+            System.out.println("Dividing is fun!");
+        }
+    }
+}
