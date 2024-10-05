131b59bdcc1d0fdc0ec4a60a355dafa018136170
Carlo Cumino <ccumino89@gmail.com>
2024-10-05 18:12:45 +0200

    Add the exercise about execeptions

--- src/Exercise12/Exercise12.java
+++ src/Exercise12/Exercise12.java
@@ -4,8 +4,9 @@ public class Exercise12 {
 
     public static void main(String[] args) {
 
+
         try{
-            int c= 30/0;
+            int c =(30/0);
             System.out.println("The result is: "+c);
         } catch (ArithmeticException e) {
             System.out.println("Impossibile dividing for 0");
